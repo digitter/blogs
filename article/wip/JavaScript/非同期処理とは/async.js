4b28ef220c3d9d8aa1e30a32ec8f038e915f5cf1
kazuki <matchingserver@gmail.com>
2020-12-13 01:59:07 +0900

    update

--- article/wip/JavaScript/非同期処理とは/async.js
+++ article/wip/JavaScript/非同期処理とは/async.js
@@ -1,18 +1,21 @@
-console.log('1')
+function showNum(num) {
+  console.log(num)
+}
 
-setTimeout(
-  function() { console.log('2') },
-  '3000'
-);
+function waitThreeSecond(num) {
+  setTimeout(
+    function() { console.log(num) },
+    '3000'
+  )
+}
 
-console.log('3')
-
-
-// つまり、JSで行う処理は非同期的な処理
-
-
-// いいね => データ保存・削除 => 画面を再描画(redirect/renderも全体を再描画)
-// いいね => データ保存・削除 => 部分的に画面描画(JSで通信をして、部分的な再描画)
-
-// jsで通信 = remote: true
-// jsで再描画 = DOMの操作
+showNum('1')
+showNum('2')
+showNum('3')
+waitThreeSecond('4')
+showNum('5')
+showNum('6')
+waitThreeSecond('7')
+showNum('8')
+showNum('9')
+waitThreeSecond('10')
